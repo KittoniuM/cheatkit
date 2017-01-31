15320c39754c0290c01bc1f9738f9a007e4923b4
Constantine <t3a@protonmail.com>
2017-02-01 00:14:06 +0200

    Error reporting

--- memf/src/main.c
+++ memf/src/main.c
@@ -1,11 +1,10 @@
+#include <assert.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include <getopt.h>
-
-#include <sys/mman.h>
 
 #include "memf.h"
 #include "lisp.h"
@@ -14,12 +13,11 @@ static const char usage[] =
 	"Usage: memf [options] <filter file>\n"
 	"Options:\n"
 	"  -h, --help                  Show this message.\n"
-	"  -p, --pid PID               Process ID to work at.\n"
-	"  -r, --range RANGE           Memf range.  Defined as from..to.  Ranges have\n"
-	"                              two aliases:\n"
-	"                                32 for 0..ffffffff;\n"
-	"                                64 for 0..7fffffffffffffff.  Default is 64.\n"
-	"  -c, --func                  Test function.  Must be written in a LISP-like\n"
+	"  -p, --pid PID               Process ID to work at, in base 10.\n"
+	"  -r, --range RANGE           Range to scan on.  Defined as from..to .  Default\n"
+	"                              is 0..7fffffffffffffff .  Input must be in base\n"
+	"                              16 and with NO 0x prefix.\n"
+	"  -f, --func                  Test function.  Must be written in a LISP-like\n"
 	"                              language.  Symbols are presented in a table\n"
 	"                              below.\n"
 	"LISP symbols:\n"
@@ -39,7 +37,8 @@ static const char usage[] =
 	"\n"
 	"  (=,!= a ...)                Test at least two numbers for equality.\n"
 	"  (f=,f!= a ...)              Test at least two numbers for equality, with\n"
-	"                              floating inaccuracy point tolerance.\n"
+	"                              tolerance for inaccuracies in floating point\n"
+	"                              numbers.\n"
 	"  (<,>,<=,>= a b)             Compare two numbers.\n"
 	"  (! a)                       Logical not.\n"
 	"  (&&,|| a b)                 Logical and and or.\n"
@@ -52,93 +51,90 @@ static const char usage[] =
 	"  [i,u][16,32,64]             Word from current address, -le or -be depending\n"
 	"                              on platform's endianness.\n"
 	"  f[32,64][le,be, ]           Single or double precision floating point\n"
-	"                              number from current address.\n"
-	;
+	"                              number from current address.\n";
 
 int main(int argc, char **argv)
 {
-	static struct option long_options[] = {
+	const struct option long_options[] = {
 		{"help",  no_argument,       NULL, 'h'},
 		{"pid",   required_argument, NULL, 'p'},
 		{"range", required_argument, NULL, 'r'},
-		{"func",  required_argument, NULL, 'c'},
+		{"func",  required_argument, NULL, 'f'},
+		/* add -s that converts everything to human-readable table */
 		{0}
 	};
+	struct memf_args args = {
+		.pid  = 0,
+		.from = 0,
+		.to   = (unsigned long) -1,
+		.prog = {0}
+	};
+	enum memf_status rc;
 	int c, option_index;
-	while ((c = getopt_long(argc, argv, "hp:r:c:",
+
+	while ((c = getopt_long(argc, argv, "hp:r:f:",
 				long_options, &option_index)) != -1) {
 		switch (c) {
 		case 'h':
 			printf(usage);
+			if (lisp_synv(&args.prog) != SYNV_EMPTY)
+				lisp_free(&args.prog);
 			return EXIT_SUCCESS;
-
 		case 'p':
+			sscanf(optarg, "%lu", &args.pid);
 			break;
-
 		case 'r':
-			/* sscanf(optarg, "%llx-%llx", &from, &to); */
+			sscanf(optarg, "%lx-%lx", &args.from, &args.to);
 			break;
-
-		case 'c':
+		case 'f':
+			if (lisp_synv(&args.prog) != SYNV_EMPTY)
+				lisp_free(&args.prog);
+			lisp_ldprog(&args.prog, optarg);
 			break;
-		}
-	}
-
-	/*
-	 * if (args.pid == 0) {
-	 * 	fprintf(stderr, "memf: error: pid is not set\n");
-	 * 	return EXIT_FAILURE;
-	 * } else if (args.type == TYPE_INVALID) {
-	 * 	fprintf(stderr, "memf: error: bad type\n");
-	 * 	return EXIT_FAILURE;
-	 * } else if (args.func == FUNC_INVALID) {
-	 * 	fprintf(stderr, "memf: error: bad function\n");
-	 * 	return EXIT_FAILURE;
-	 * } else if (args.to <= args.from) {
-	 * 	fprintf(stderr, "memf: error: bad range\n");
-	 * 	return EXIT_FAILURE;
-	 * }
-	 */
-
-	lisp_program_t prog;
-	lisp_ldprog(&prog, "(= u32 ()");
-	size_t col;
-	switch (lisp_synv(&prog, &col)) {
-	case SYNV_ILLTOKEN:
-		printf("memf: error: bad token at %d\n", col);
-		break;
-	case SYNV_BRACKET:
-		printf("memf: error: unmatched bracket at %d\n", col);
-		break;
-	}
-	for (size_t i = 0; i < prog.num_tokens; i++) {
-		lisp_token_t *tok = &prog.tokens[i];
-		switch (tok->type) {
-		case TOK_BEG:
-			printf("(\n");
-			break;
-		case TOK_END:
-			printf(")\n");
-			break;
-		case TOK_SYM:
-			printf("sym %s\n", tok->value.str);
-			break;
-		case TOK_FLT:
-			printf("flt %f\n", tok->value.flt);
-			break;
-		case TOK_INT:
-			printf("int %d\n", (int) tok->value.sint);
-			break;
-		case TOK_STR:
-			printf("str %s\n", tok->value.str);
-			break;
+		case '?':
+			goto fail;
 		default:
-			printf("??\n");
-			break;
+			assert(0);
 		}
 	}
-	lisp_free(&prog);
 
-	/* enum memf_status rc = memf(&args); */
+	if (args.pid == 0) {
+		fprintf(stderr, "memf: error: pid is not set\n");
+		goto fail;
+	}
+	if (args.from >= args.to) {
+		fprintf(stderr, "memf: error: bad range\n");
+		goto fail;
+	}
+
+	switch (lisp_synv(&args.prog)) {
+	case SYNV_EMPTY:
+		fprintf(stderr, "memf: error: program is empty\n");
+		goto fail;
+	case SYNV_SCOPE:
+		fprintf(stderr, "memf: error: missed a bracket\n");
+		goto fail;
+	case SYNV_ILLTOK:
+		fprintf(stderr, "memf: error: illegal token\n");
+		goto fail;
+	default:
+		break;
+	}
+
+	switch (rc = memf(&args)) {
+	case MEMF_OK:
+		break;
+	case MEMF_ERR_PROC:
+		fprintf(stderr, "memf: error: procfs error\n");
+		goto fail;
+	case MEMF_ERR_IO:
+		fprintf(stderr, "memf: error: nigger error\n");
+		goto fail;
+	}
+	lisp_free(&args.prog);
 	return EXIT_SUCCESS;
+fail:
+	if (lisp_synv(&args.prog) != SYNV_EMPTY)
+		lisp_free(&args.prog);
+	return EXIT_FAILURE;
 }
